2856de05db3b789e365d0f0cedaef4adf69f4fb8
Dai,Peng-Yang <pengyangdai@kkbox.com>
2019-12-04 17:56:25 +0800

    [feat]: implemented `protocol CompatibleContextMenuTableView`

--- Sources/CompatibleContextMenuInteraction/CompatibleContextMenuInteraction.swift
+++ Sources/CompatibleContextMenuInteraction/CompatibleContextMenuInteraction.swift
@@ -1,36 +1,70 @@
 import UIKit
-@objcMembers class KKPreviewModel: NSObject {
-	let previewingViewController: UIViewController
-	// FIXME: deal with `.null` value
-	/// For Objective-C compatibility, `.null` means nil
-	let originatedFrom: CGRect
-	let actions: [KKPreviewAction]
-	init(previewing viewController: UIViewController, originatedFrom: CGRect = .null, actions: [KKPreviewAction] = []) {
-		self.previewingViewController = viewController
-		self.originatedFrom = originatedFrom
-		self.actions = actions
-		super.init()
-	}
-}
 
-@objcMembers class KKPreviewModelForTableView: KKPreviewModel {
-	let pointInCell: CGPoint
-	let indexPath: IndexPath
+// MARK: - Model -
+public struct Model {
+	public let previewingViewController: UIViewController
+	public let originatedFrom: CGRect?
 	
-	init(_ mode: KKPreviewModel, indexPath: IndexPath, pointInCell: CGPoint) {
-		self.indexPath = indexPath
-		self.pointInCell = pointInCell
-		super.init(previewing: mode.previewingViewController, originatedFrom: mode.originatedFrom, actions: mode.actions)
+	public struct Action {
+		public let title: String
+		public let destructive: Bool
+		public let handler: () -> Void
+	}
+	public let actions: [Action] = []
+	
+	public enum Commit {
+		public typealias Handler = (UIViewController) -> Void
+		case show, showDetail, custom(Handler?)
+	}
+	public let commit: Commit
+}
+
+public extension UIPreviewAction {
+	convenience init(_ action: Model.Action) {
+		self.init(title: action.title, style: action.destructive ? .destructive : .default) { _, _ in action.handler() }
 	}
 }
 
-/// TODO: remove UIPreviewActionItem conformance as UIKit definitly misuse the design of protocol
-@objcMembers class KKPreviewAction: NSObject, UIPreviewActionItem {
-	typealias Handler = () -> Void
-	let title: String
-	let destructive: Bool
-	let handler: Handler
-	init(title: String, destructive: Bool = false, handler: @escaping Handler) {
+struct IndexedViewCellModel {
+	let model: Model
+	let indexPath: IndexPath
+	let pointInCell: CGPoint
+}
+
+// MARK: Bridge
+@available(iOS 13, *)
+public extension UIAction {
+	convenience init(_ action: Model.Action) {
+		self.init(title: action.title, attributes: action.destructive ? [.destructive] : []) { _ in action.handler() }
+	}
+}
+
+@available(iOS 13, *)
+public extension UIMenu {
+	convenience init(actions: [Model.Action]) {
+		self.init(title: "", children: actions.map { UIAction($0) })
+	}
+}
+
+@available(iOS 13, *)
+public extension UITargetedPreview {
+	@objc convenience init(view: UIView, rounded rect: CGRect, cornerRadius: CGFloat = 3) {
+		let parameters = UIPreviewParameters()
+		parameters.visiblePath = .init(roundedRect: rect, cornerRadius: cornerRadius)
+		parameters.backgroundColor = .clear
+		
+		self.init(view: view, parameters: parameters)
+	}
+}
+
+// TODO: rewrite in objective-c to make swift unavialable
+@available(*, unavailable, renamed: "Model.Action")
+@objcMembers final public class CompatibleContextMenuModelAction: NSObject {
+	public typealias Handler = () -> Void
+	public let title: String
+	public let destructive: Bool
+	public let handler: Handler
+	public init(title: String, destructive: Bool = false, handler: @escaping Handler) {
 		self.title = title
 		self.destructive = destructive
 		self.handler = handler
@@ -38,6 +72,161 @@ import UIKit
 	}
 }
 
-struct CompatibleContextMenuInteraction {
-    var text = "Hello, World!"
+
+// MARK: - Compatible Protocols -
+// MARK: - UIView
+public protocol ViewDelegate {
+	func model(in view: UIView, at point: CGPoint) -> Model?
 }
+
+public protocol CompatibleContextMenuView: UIView {
+	func registerForCompatibleContextMenu(with delegate: ViewDelegate)
+}
+
+// MARK: - UITableView
+public protocol TableViewDelegate: UITableViewController {
+	func model(in tableView: UITableView, on indexPath: IndexPath, at pointInCell: CGPoint) -> Model?
+}
+
+public protocol CompatibleContextMenuTableView: UITableView, UIViewControllerPreviewingDelegate, UIContextMenuInteractionDelegate {
+	var compatibleContextMenuDelegate: TableViewDelegate? { get set }
+}
+
+public extension CompatibleContextMenuTableView {
+	// TODO: property wrapper
+	var compatibleContextMenuDelegate: TableViewDelegate? {
+		get { storage?.delegate }
+		set {
+			guard compatibleContextMenuDelegate !== newValue else { return }
+			
+			if let storage = storage {
+				storage.delegate.unregisterForPreviewing(withContext: storage.context)
+				if #available(iOS 13.0, *) {
+					removeInteraction(UIContextMenuInteraction(delegate: self))
+				}
+			}
+			
+			guard let newValue = newValue else {
+				return storage = nil
+			}
+			let context = newValue.registerForPreviewing(with: self, sourceView: self)
+			if #available(iOS 13.0, *) {
+				addInteraction(UIContextMenuInteraction(delegate: self))
+			}
+			storage = Storage(delegate: newValue, context: context)
+		}
+	}
+}
+
+// MARK: Storage
+private extension UITableView {
+	typealias StorageType = Storage<TableViewDelegate>
+	static var contextMenuStorage = [UITableView: StorageType]()
+	var storage: StorageType? {
+		get { Self.contextMenuStorage[self] }
+		set { Self.contextMenuStorage[self] = newValue }
+	}
+}
+
+// MARK: Convenient Functions
+private extension CompatibleContextMenuTableView {
+	func commit(delegate: TableViewDelegate, model: Model) {
+		let viewController = model.previewingViewController
+		switch model.commit {
+		case .show: delegate.show(viewController, sender: self)
+		case .showDetail: delegate.showDetailViewController(viewController, sender: self)
+		case let .custom(handler): handler?(viewController)
+		}
+	}
+	
+	@available(iOS 13.0, *)
+	var targetedPreview: UITargetedPreview? {
+		guard
+			let model = storage?.model,
+			let cell = cellForRow(at: model.indexPath) else { return nil }
+		
+		if let originatedRect = model.model.originatedFrom {
+			return .init(view: cell, rounded: originatedRect)
+		} else {
+			return .init(view: cell)
+		}
+	}
+}
+
+// MARK: UIViewControllerPreviewingDelegate
+public extension CompatibleContextMenuTableView {
+	/// - parameter location: location in table view
+	/// - returns: point in cell and index path of such cell
+	private func convert(_ location: CGPoint) -> (indexPath: IndexPath, location: CGPoint)? {
+		guard
+			let indexPath = indexPathForRow(at: location),
+			let cell = cellForRow(at: indexPath) else { return nil }
+		return (indexPath, convert(location, to: cell))
+	}
+	
+	func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+		guard
+			let storage = storage,
+			let cellInfo = convert(location),
+			let model = storage.delegate.model(in: self, on: cellInfo.indexPath, at: cellInfo.location) else { return nil }
+		storage.model = IndexedViewCellModel(model: model, indexPath: cellInfo.indexPath, pointInCell: cellInfo.location)
+		
+		if let sourceRect = model.originatedFrom {
+			previewingContext.sourceRect = sourceRect
+		}
+		return model.previewingViewController
+	}
+	
+	func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+		guard
+			let storage = storage,
+			let model = storage.model else { return }
+		assert(model.model.previewingViewController === viewControllerToCommit)
+		commit(delegate: storage.delegate, model: model.model)
+	}
+}
+
+// MARK: UIContextMenuInteractionDelegate
+@available(iOS 13.0, *)
+public extension CompatibleContextMenuTableView {
+	func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+		guard
+			let storage = storage,
+			let cellInfo = convert(location),
+			let model = storage.delegate.model(in: self, on: cellInfo.indexPath, at: cellInfo.location) else { return nil }
+		storage.model = IndexedViewCellModel(model: model, indexPath: cellInfo.indexPath, pointInCell: cellInfo.location)
+		
+		return .init(identifier: nil,
+					 previewProvider: { model.previewingViewController },
+					 actionProvider: { _ in UIMenu(actions: model.actions) }
+		)
+	}
+	
+	func contextMenuInteraction(_ interaction: UIContextMenuInteraction, previewForDismissingMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
+		targetedPreview
+	}
+	
+	func contextMenuInteraction(_ interaction: UIContextMenuInteraction, previewForHighlightingMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
+		targetedPreview
+	}
+	
+	func contextMenuInteraction(_ interaction: UIContextMenuInteraction, willPerformPreviewActionForMenuWith configuration: UIContextMenuConfiguration, animator: UIContextMenuInteractionCommitAnimating) {
+		guard
+			let storage = storage,
+			let model = storage.model else { return }
+		assert(model.model.previewingViewController === animator.previewViewController)
+		commit(delegate: storage.delegate, model: model.model)
+	}
+}
+
+// MARK: UICollectionView
+public protocol CollectionViewDelegate {
+	func model(in collectionView: UICollectionView, on indexPath: IndexPath, at pointInCell: CGPoint) -> Model?
+}
+
+public protocol CompatibleContextMenuCollectionView {
+	var contextMenuDelegate: CollectionViewDelegate { get }
+}
+
+}
+
