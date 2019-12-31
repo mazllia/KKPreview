mazllia/KKPreview

commit ab3d495745b471ea35f9c4683f1a2b69535bf79e

[fix]: release the previewing view controller when dismiss the preview
KKPreview now release the previewing view controller right after it is passed to UIKit

Sources/KKPreview/CollectionView.swift
@@ -6,8 +6,7 @@ import Foundation
 		guard
 			let storage: InteractivePreviewStorage = indexViewStorage ?? viewStorage,
 			let model = storage.model?.model else { return }
-		assert(model.previewingViewController === viewControllerToCommit)
-		storage.presentingViewController?.commit(model)
+		storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)
 		storage.model = nil
 	}
 }
@@ -17,8 +16,9 @@ import Foundation
 @objc extension UICollectionView {
 	public override func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
 		guard let model = askDelegateToUpdateStoredPreviewModel(at: location) else { return nil }
+		defer { model.previewingViewController = nil }
 		return .init(identifier: nil,
-					 previewProvider: { model.previewingViewController },
+					 previewProvider: { [previewingViewController = model.previewingViewController] in previewingViewController },
 					 actionProvider: { _ in UIMenu(actions: model.actions) }
 		)
 	}
@@ -46,7 +46,9 @@ import Foundation
 			let model = storage.model?.model else { return }
 		assert(model.previewingViewController === animator.previewViewController)
 		animator.addCompletion {
-			storage.presentingViewController?.commit(model)
+			if let viewControllerToCommit = animator.previewViewController {
+				storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)
+			}
 			storage.model = nil
 		}
 	}

Sources/KKPreview/KKPreview.swift
@@ -3,7 +3,8 @@ import UIKit
 // MARK: - Model -
 @objcMembers
 final public class KKPreviewModel: NSObject {
-	public let previewingViewController: UIViewController
+	/// This is released right after being passed to UIKit.
+	public internal(set) var previewingViewController: UIViewController?
 	
 	public let originatedFrom: CGRect?
 	@available(swift, obsoleted: 1)
@@ -274,12 +275,11 @@ extension Storage: InteractivePreviewStorage {
 }
 
 @objc public extension UIViewController {
-	func commit(_ model: KKPreviewModel) {
-		let viewController = model.previewingViewController
-		switch model.commit.style {
+	func commit(_ commit: KKPreviewCommit, to viewController: UIViewController) {
+		switch commit.style {
 		case .show: show(viewController, sender: self)
 		case .showDetail: showDetailViewController(viewController, sender: self)
-		case .custom: model.commit.handler?(viewController)
+		case .custom: commit.handler?(viewController)
 		}
 	}
 }

Sources/KKPreview/TableView.swift
@@ -6,8 +6,7 @@ import Foundation
 		guard
 			let storage: InteractivePreviewStorage = indexViewStorage ?? viewStorage,
 			let model = storage.model?.model else { return }
-		assert(model.previewingViewController === viewControllerToCommit)
-		storage.presentingViewController?.commit(model)
+		storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)
 		storage.model = nil
 	}
 }
@@ -17,8 +16,9 @@ import Foundation
 @objc extension UITableView {
 	public override func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
 		guard let model = askDelegateToUpdateStoredPreviewModel(at: location) else { return nil }
+		defer { model.previewingViewController = nil }
 		return .init(identifier: nil,
-					 previewProvider: { model.previewingViewController },
+					 previewProvider: { [previewingViewController = model.previewingViewController] in previewingViewController },
 					 actionProvider: { _ in UIMenu(actions: model.actions) }
 		)
 	}
@@ -44,9 +44,10 @@ import Foundation
 		guard
 			let storage: InteractivePreviewStorage = indexViewStorage ?? viewStorage,
 			let model = storage.model?.model else { return }
-		assert(model.previewingViewController === animator.previewViewController)
 		animator.addCompletion {
-			storage.presentingViewController?.commit(model)
+			if let viewControllerToCommit = animator.previewViewController {
+				storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)
+			}
 			storage.model = nil
 		}
 	}

Sources/KKPreview/View.swift
@@ -7,6 +7,7 @@ import Foundation
 		if let sourceRect = model.originatedFrom {
 			previewingContext.sourceRect = sourceRect
 		}
+		defer { model.previewingViewController = nil }
 		return model.previewingViewController
 	}
 	
@@ -14,8 +15,7 @@ import Foundation
 		guard
 			let storage: InteractivePreviewStorage = viewStorage,
 			let model = storage.model?.model else { return }
-		assert(model.previewingViewController === viewControllerToCommit)
-		storage.presentingViewController?.commit(model)
+		storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)
 		storage.model = nil
 	}
 }
@@ -25,8 +25,9 @@ import Foundation
 @objc extension UIView: UIContextMenuInteractionDelegate {
 	public func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
 		guard let model = askDelegateToUpdateStoredPreviewModel(at: location) else { return nil }
+		defer { model.previewingViewController = nil }
 		return .init(identifier: nil,
-					 previewProvider: { model.previewingViewController },
+					 previewProvider: { [previewingViewController = model.previewingViewController] in previewingViewController },
 					 actionProvider: { _ in UIMenu(actions: model.actions) }
 		)
 	}
@@ -48,9 +49,10 @@ import Foundation
 		guard
 			let storage: InteractivePreviewStorage = viewStorage,
 			let model = storage.model?.model else { return }
-		assert(model.previewingViewController === animator.previewViewController)
 		animator.addCompletion {
-			storage.presentingViewController?.commit(model)
+			if let viewControllerToCommit = animator.previewViewController {
+				storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)
+			}
 			storage.model = nil
 		}
 	}

commit 8d03d3742660adcf38cdb612647ee306e637c5af

[fix]: CollectionView crashes when commit introduced in ab3d495745b471ea35f9c4683f1a2b69535bf79e

Sources/KKPreview/CollectionView.swift
@@ -44,7 +44,6 @@ import Foundation
 		guard
 			let storage: InteractivePreviewStorage = indexViewStorage ?? viewStorage,
 			let model = storage.model?.model else { return }
-		assert(model.previewingViewController === animator.previewViewController)
 		animator.addCompletion {
 			if let viewControllerToCommit = animator.previewViewController {
 				storage.presentingViewController?.commit(model.commit, to: viewControllerToCommit)